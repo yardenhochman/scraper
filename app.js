9980b2496442aa98dfdfd10f90ed5507bb5589f7
Yarden Hochman <yyyarden@gmail.com>
2017-12-29 17:04:35 -0500

    added title

--- app.js
+++ app.js
@@ -3,50 +3,76 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 
-url = 'https://www.acropedia.org/poses/?_sft_position=l-base';
+url =
+  'https://www.acropedia.org/poses/?_sft_position=l-base';
 
 request(url, function(error, response, html) {
-  if(error) return;
-  
+  if (error) return;
+
   var $ = cheerio.load(html);
 
   var json = [];
   var pagePromises = [];
   var posePromises = [];
 
-  $('.pagination a').each(function () {
+  $('.pagination a').each(function() {
     getPoses($(this).attr('href'));
   });
 
   function getPoses(url) {
-    pagePromises.push(new Promise((suc) => {
-      request(url, function(error, response, html) {
-        $('.post-item .caption a').each(function () {
-          var href = $(this).attr('href');
+    pagePromises.push(
+      new Promise(suc => {
+        request(url, function(
+          error,
+          response,
+          html
+        ) {
+          $('.post-item .caption a').each(
+            function() {
+              var href = $(this).attr('href');
 
-          posePromises.push(new Promise((suc) => {
-            request(href, (error, response, html) => {
-              var $ = cheerio.load(html);
+              posePromises.push(
+                new Promise(suc => {
+                  request(
+                    href,
+                    (error, response, html) => {
+                      var $ = cheerio.load(html);
 
-              var fields = {
-                img : $('.tiled-gallery-item img').attr('data-orig-file')
-              };
+                      var fields = {
+                        img: $(
+                          '.tiled-gallery-item img'
+                        ).attr('data-orig-file'),
+                        title: $(
+                          '.breadcrumb_last').text()
+                        
+                      };
 
-              $('.acro-meta-fields tr').each(function () {
-                fields[$(this).find('.label').text()] = $(this).find('.data').text();
-              });
+                      $(
+                        '.acro-meta-fields tr'
+                      ).each(function() {
+                        fields[
+                          $(this)
+                            .find('.label')
+                            .text()
+                        ] = $(this)
+                          .find('.data')
+                          .text();
+                      });
 
-              json.push(fields);
+                      json.push(fields);
+                      suc();
+                    }
+                  );
+                })
+              );
+
               suc();
-            });
-          }));
-
-          suc();
+            }
+          );
         });
-      });
-    }));
+      })
+    );
   }
-
 
   Promise.all(pagePromises)
     .then(() => {
